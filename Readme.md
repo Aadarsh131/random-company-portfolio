5cbc8089fe37ee551d531eef89bb204783bfe944
aadarsh <aadarshkumar131@gmail.com>
2024-06-02 19:28:12 +0530

    second commit

--- Readme.md
+++ Readme.md
@@ -1,3 +1,6 @@
 # TODO
-- attach correct Cormorant SC font
+- attach quotemark image in 'feedback' section
 - change px to relevant unit for responsivenesss
+- change the images for our project section (keep some dark outlined images)
+- try to reuse the code of 'our project' section for changning the image logic on click
+- try to adjust the icons height and width according to the design
--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 $(document).ready(function () {
-  // $(".contact-btn").click(function () {
-  //   // $("p").hide();
-
-  // });
-  $('#exampleModalCenter').modal('show');
-//   $('#myModal').on('shown.bs.modal', function() {
-//   console.log('Modal shown');
-// });
-
-
-// $('#myModal').on('hidden.bs.modal', function() {
-//   console.log('Modal hidden');
-// });
+    // $("#project-image2").hide()
+    // $("#project-image3").hide()
+    $('#project-1').click(function () {
+        $("#project-image1").show()
+        $(this).css({'background-color': '#FF3147', 'color': '#FFFFFF'});
+        $('#project-2').css({'background-color': '#F6F6F6', 'color': '#000000'});
+        $('#project-3').css({'background-color': '#F6F6F6', 'color': '#000000'});
+        $("#project-image2").hide()
+        $("#project-image3").hide()
+    })
+    $('#project-2').click(function () {
+        $(this).css({'background-color': '#FF3147', 'color': '#FFFFFF'});
+        $('#project-1').css({'background-color': '#F6F6F6', 'color': '#000000'});
+        $('#project-3').css({'background-color': '#F6F6F6', 'color': '#000000'});
+        $("#project-image1").hide()
+        $("#project-image2").show()
+        $("#project-image3").hide()
+    })
+    $('#project-3').click(function () {
+        $(this).css({'background-color': '#FF3147', 'color': '#FFFFFF'});
+        $('#project-1').css({'background-color': '#F6F6F6', 'color': '#000000'});
+        $('#project-2').css({'background-color': '#F6F6F6', 'color': '#000000'});
+        $("#project-image1").hide()
+        $("#project-image2").hide()
+        $("#project-image3").show()
+    })
 });
